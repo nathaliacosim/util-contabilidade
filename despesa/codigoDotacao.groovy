1fc225a8b2abf49a4ee44f1ec23c5de0a267c82c
nathaliacosim <nathalia.cosim@novaandradina.org>
2022-11-10 11:43:14 -0400

    Adequações

--- despesa/codigoDotacao.groovy
+++ despesa/codigoDotacao.groovy
@@ -5,6 +5,7 @@ def programa    = "0803"
 def acao        = "2019"
 def natureza    = "33903900"
 def recurso     = "0.1.0046.0000"
+def exercicio   = 2022
 
 def getRight = { str, tam ->
    def tamStr = str.length()
@@ -23,9 +24,18 @@ def numPrograma  = programa.toString().take(4).padLeft(4, '0')
 def numAcao      = acao.toString().take(1) + "0" + getRight(acao.toString().substring(1), 3).padLeft(3,'0')
 def numNatureza  = "${natureza.toString().substring(0, 1)}.${natureza.toString().substring(1, 2)}.${natureza.toString().substring(2, 4)}.${natureza.toString().substring(4, 6)}.${natureza.toString().substring(6, 8)}"
 def arrRecurso   = recurso.split("\\.").collect({ it.toString() })
-def numRecurso   = arrRecurso.size() >= 3 ? (arrRecurso[0].padRight(1,'0') + "." + arrRecurso[1].padRight(1,'0') + "." + getRight(arrRecurso[2], 2).padRight(2,'0') + "." + arrRecurso[3].padRight(6,'0'))  : ""
+if(exercicio < 2022){
+   numRecurso = ( arrRecurso.size() >= 4 ? (arrRecurso[0].padRight(1,'0') + "." + arrRecurso[1].padRight(1,'0') + "." + getRight(arrRecurso[2], 2).padRight(2,'0') + "." + arrRecurso[3].padRight(6,'0')) : "" )
+} else {
+   numRecurso = ( arrRecurso.size() >= 3 ? (arrRecurso[0].padRight(1,'0') + "." + arrRecurso[1].padRight(3,'0') + "." + getRight(arrRecurso[2], 7).padRight(7,'0'))  : "" )
+}
 
 def DOT_CodigoResumido = """${numOrgao}.${numUnidade}.${numFuncao}.${numSubfuncao}.${numPrograma}.${numAcao}.${numNatureza}.${numRecurso}"""
-def modeloIdeal        = "99.999.99.999.9999.99999.9.9.99.99.99.9.9.99.999999"
-println modeloIdeal
+
+def modeloIdeal2021    = "99.999.99.999.9999.99999.9.9.99.99.99.9.9.99.999999"
+def modeloIdeal2022    = "99.999.99.999.9999.99999.9.9.99.99.99.9.999.9999999"
+
+println modeloIdeal2021
+println modeloIdeal2022
+
 println DOT_CodigoResumido 
